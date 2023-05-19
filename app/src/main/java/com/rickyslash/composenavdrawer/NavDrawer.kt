5eacd242fb9959fe2234af721316bd106b355764
ricky-kiva <ricky.kiva@gmail.com>
2023-05-19 20:56:23 +0700

    add Drawer Items

--- app/src/main/java/com/rickyslash/composenavdrawer/NavDrawer.kt
+++ app/src/main/java/com/rickyslash/composenavdrawer/NavDrawer.kt
@@ -1,19 +1,30 @@
 package com.rickyslash.composenavdrawer
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rickyslash.composenavdrawer.ui.theme.ComposeNavDrawerTheme
 import kotlinx.coroutines.launch
+
+data class MenuItem(
+    val title: String,
+    val icon: ImageVector
+)
 
 @Composable
 fun NavDrawer() {
@@ -29,7 +40,7 @@ fun NavDrawer() {
                 }
             })
         },
-        drawerContent = { Text(stringResource(R.string.hello_from_nav_drawer)) },
+        drawerContent = { MyDrawerContent {} },
         drawerGesturesEnabled = scaffoldState.drawerState.isOpen
     ) { paddingValues ->
         Box(
@@ -38,6 +49,37 @@ fun NavDrawer() {
                 .padding(paddingValues),
             contentAlignment = Alignment.Center
         ) { Text(stringResource(R.string.hello_world)) }
+    }
+}
+
+@Composable
+fun MyDrawerContent(
+    modifier: Modifier = Modifier,
+    onItemSelected: (title: String) -> Unit
+) {
+    val items = listOf(
+        MenuItem(stringResource(R.string.home), Icons.Default.Home),
+        MenuItem(stringResource(R.string.favourite), Icons.Default.Favorite),
+        MenuItem(stringResource(R.string.profile), Icons.Default.AccountCircle)
+    )
+    Column(modifier.fillMaxSize()) {
+        Box(modifier = Modifier
+            .height(190.dp)
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.primary))
+        for (item in items) {
+            Row(modifier = Modifier
+                .clickable { onItemSelected(item.title) }
+                .padding(vertical = 12.dp, horizontal = 16.dp)
+                .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = item.icon, contentDescription = item.title)
+                Spacer(modifier = Modifier.width(32.dp))
+                Text(text = item.title, style = MaterialTheme.typography.subtitle2)
+            }
+        }
+        Divider()
     }
 }
 
